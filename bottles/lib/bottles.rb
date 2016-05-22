51a60fac3e656daa5a3f54b0f89d1a6dd59ecdec
Richard Jordan <richarddjordan@gmail.com>
2016-05-22 14:12:19 -0700

    make test one pass by just copying in the lyric

--- bottles/lib/bottles.rb
+++ bottles/lib/bottles.rb
@@ -1,3 +1,9 @@
 class Bottles
 
+  def verse(count)
+    "#{count} bottles of beer on the wall, " +
+      "#{count} bottles of beer.\n" +
+      "Take one down and pass it around, " +
+      "#{count - 1} bottles of beer on the wall.\n"
+  end
 end
