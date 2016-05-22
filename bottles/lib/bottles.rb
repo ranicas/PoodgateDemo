f0557c668b21b3dd3c86ab05da848e7c7cfa865c
Richard Jordan <richarddjordan@gmail.com>
2016-05-22 14:20:21 -0700

    make test four pass

--- bottles/lib/bottles.rb
+++ bottles/lib/bottles.rb
@@ -1,9 +1,16 @@
 class Bottles
 
   def verse(count)
+    if count == 1
+      "1 bottle of beer on the wall, " +
+        "1 bottle of beer.\n" +
+        "Take it down and pass it around, " +
+        "no more bottles of beer on the wall.\n"
+    else
     "#{count} bottles of beer on the wall, " +
       "#{count} bottles of beer.\n" +
       "Take one down and pass it around, " +
       "#{count - 1} bottle#{(count > 2) ? 's' : ''} of beer on the wall.\n"
+    end
   end
 end
--- bottles/test/bottles_test.rb
+++ bottles/test/bottles_test.rb
@@ -27,7 +27,6 @@ class BottlesTest < Minitest::Test
   end
 
   def test_verse_1
-    skip
     expected = "1 bottle of beer on the wall, " +
       "1 bottle of beer.\n" +
       "Take it down and pass it around, " +
