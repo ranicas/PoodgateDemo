b409398ae96927ecf3003ec20d050c1a3894a4d0
Richard Jordan <richarddjordan@gmail.com>
2016-05-22 14:47:03 -0700

    refactor step one

--- bottles/lib/bottles.rb
+++ bottles/lib/bottles.rb
@@ -10,21 +10,28 @@ class Bottles
   end
 
   def verse(count)
+    quantity = case count
+                 when 0 then "No more bottles"
+                 when 1 then "1 bottle"
+                 else "#{count} bottles"
+               end
+
     if count == 0
-      "No more bottles of beer on the wall, " +
-        "no more bottles of beer.\n" +
+      "#{quantity} of beer on the wall, " +
+        "#{quantity.downcase} of beer.\n" +
         "Go to the store and buy some more, " +
         "99 bottles of beer on the wall.\n"
     elsif count == 1
-      "1 bottle of beer on the wall, " +
-        "1 bottle of beer.\n" +
+      "#{quantity} of beer on the wall, " +
+        "#{quantity.downcase} of beer.\n" +
         "Take it down and pass it around, " +
         "no more bottles of beer on the wall.\n"
     else
-      "#{count} bottles of beer on the wall, " +
-        "#{count} bottles of beer.\n" +
+      "#{quantity} of beer on the wall, " +
+        "#{quantity.downcase} of beer.\n" +
         "Take one down and pass it around, " +
         "#{count - 1} bottle#{(count > 2) ? 's' : ''} of beer on the wall.\n"
     end
   end
+
 end
